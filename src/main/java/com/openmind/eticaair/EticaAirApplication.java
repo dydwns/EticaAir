2aea2727709bd27f002937926a731b1ab113a199
dydwns <kaka2438@hanmail.net>
2024-08-11 19:12:15 +0900

    🎉 프로젝트 뼈대1

---
+++ src/main/java/com/openmind/eticaair/EticaAirApplication.java
@@ -0,0 +1,13 @@
+package com.openmind.eticaair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EticaAirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EticaAirApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/openmind/eticaair/EticaAirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openmind.eticaair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EticaAirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
